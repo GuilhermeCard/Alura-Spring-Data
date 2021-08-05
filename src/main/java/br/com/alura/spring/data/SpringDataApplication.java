831c009468addb8dc9d8eabb2e698a1bc26b96f5
Guilherme Cardoso <guilhermemelocardoso@gmail.com>
2021-08-05 00:06:14 -0300

    aula - Primeiro Repositório

---
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.spring.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CargoRepository repository;
+	
+	public void SpringApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("desenvolvedor de software");
+		
+		repository.save(cargo);
+	}
+
+}
